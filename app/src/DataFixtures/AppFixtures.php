951f3033560f2af451db24ae6c367dc5b6594ab0
JoAnisky <anisky5@gmail.com>
2024-02-06 14:18:43 +0100

    Created users role USER and ADMIN

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Création d'un user avec le "ROLE_USER" uniquement
+        $user = new User();
+        $user->setLogin("user")
+            ->setRoles(['ROLE_USER'])
+            ->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
+        $manager->persist($user);
+
+        // Création d'un user avec le ROLE_ADMIN
+        $userAdmin = new User();
+        $userAdmin->setLogin("admin")
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->userPasswordHasher->hashPassword($userAdmin, "admin"));
+        $manager->persist($userAdmin);
 
         $manager->flush();
     }
