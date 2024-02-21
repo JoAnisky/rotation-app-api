b155fa258b5e19b7d5309a32e1e9f374bf9c66a2
10Torsions <github@10torsions.com>
2024-02-21 13:55:48 +0100

    Created method that returns JsonData into a json file

--- src/Service/ActivityService.php
+++ src/Service/ActivityService.php
@@ -5,14 +5,20 @@ declare(strict_types=1);
 namespace App\Service;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class ActivityService
 {
     private EntityManagerInterface $entityManager;
+    private $projectDir;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, KernelInterface $kernel)
     {
         $this->entityManager = $entityManager;
+        $this->projectDir = $kernel->getProjectDir();
     }
     /**
      * Nullifies the activity relationship for a collection of entities and updates the database.
@@ -27,5 +33,24 @@ class ActivityService
             $this->entityManager->persist($entity);
         }
         $this->entityManager->flush();
+    }
+
+    public function generateJson($jsonData): Response
+    {
+        dd($jsonData);
+        // $data = [
+        //     'name' => 'John Doe',
+        //     'age' => 30,
+        //     'email' => 'john@example.com'
+        // ];
+
+        $response = new Response($jsonData);
+
+        // Enregistrement du contenu JSON dans un fichier
+        $filePath = $this->projectDir . '/public/scenarios/activity.json'; // Chemin où vous souhaitez enregistrer le fichier JSON
+        file_put_contents($filePath, $jsonData);
+
+        // Réponse de confirmation
+        return new Response('Fichier JSON généré avec succès');
     }
 }
