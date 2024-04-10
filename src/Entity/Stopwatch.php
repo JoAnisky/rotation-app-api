366123575f8e797282e21427a203b0f4f63abe86
JoAnisky <anisky5@gmail.com>
2024-04-10 10:25:14 +0200

    added counter field to Stopwatch Entity

--- src/Entity/Stopwatch.php
+++ src/Entity/Stopwatch.php
@@ -24,6 +24,9 @@ class Stopwatch
     #[Groups(["getStopwatch"])]
     private ?Activity $activity = null;
 
+    #[ORM\Column(type: Types::BIGINT, nullable: true)]
+    private ?string $counter = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -49,6 +52,18 @@ class Stopwatch
     public function setActivity(?Activity $activity): static
     {
         $this->activity = $activity;
+
+        return $this;
+    }
+
+    public function getCounter(): ?string
+    {
+        return $this->counter;
+    }
+
+    public function setCounter(?string $counter): static
+    {
+        $this->counter = $counter;
 
         return $this;
     }
