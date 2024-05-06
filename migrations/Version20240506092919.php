4d669a6fc66a6e0a9271d7a0d38c6920659dc433
JoAnisky <anisky5@gmail.com>
2024-05-06 11:48:24 +0200

    removed "nb_participant" and added "participant_code" and "animator_code"

---
+++ migrations/Version20240506092919.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240506092919 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE activity ADD participant_code VARCHAR(6) NOT NULL, ADD animator_code VARCHAR(6) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_AC74095AA3A122A6 ON activity (participant_code)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_AC74095AA6F6F0E7 ON activity (animator_code)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_AC74095AA3A122A6 ON activity');
+        $this->addSql('DROP INDEX UNIQ_AC74095AA6F6F0E7 ON activity');
+        $this->addSql('ALTER TABLE activity ADD nb_participants INT DEFAULT NULL, DROP participant_code, DROP animator_code');
+    }
+}
