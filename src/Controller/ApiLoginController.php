90304f1140f904a7a676522bfb07096f899baa24
JoAnisky <anisky5@gmail.com>
2024-05-14 11:19:16 +0200

    updated LoginController to store token jwt

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -3,30 +3,48 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Validator\Constraints\Json;
 
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'app_login')]
-    public function login(TokenStorageInterface $tokenStorage): JsonResponse
+    public function login(Request $request, TokenStorageInterface $tokenStorage): JsonResponse
     {
         $token = $tokenStorage->getToken();
 
-        if(!$token){
-            return new JsonResponse(['message' => 'token manquant'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        // Récupérer le token JWT de la requête
+        $jwt = $request->headers->get('Authorization');
+
+        if (!$jwt || !str_starts_with($jwt, 'Bearer ')) {
+            return new JsonResponse(['message' => 'Token manquant ou invalide'], JsonResponse::HTTP_BAD_REQUEST);
         }
+
+        $jwt = substr($jwt, 7); // Supprimer "Bearer " pour obtenir le token JWT
+
+
+        // Create a cookie
+        $cookie = Cookie::create('authToken')
+            ->withValue($jwt)
+            ->withExpires(time() + (7 * 24 * 60 * 60)) // 7 days
+            ->withSecure(true)
+            ->withHttpOnly(false) // If true, we cant get it in frontend
+            ->withSameSite('strict');
 
         /** @var User $user */
         $user = $token->getUser();
 
-        return $this->json([
+        $response = new JsonResponse([
             'username' => $user->getLogin(),
             'user_id' => $user->getId(),
             'role' => $user->getRoles(),
         ]);
+
+        $response->headers->setCookie($cookie);
+
+        return $response;
     }
 }
