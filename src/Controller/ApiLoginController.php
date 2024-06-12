46f6e13dd5707292ec126cf8a3a1ee3c5f870d07
JoAnisky <anisky5@gmail.com>
2024-06-12 15:44:48 +0200

    Added csrf token and cookie set

--- src/Controller/ApiLoginController.php
+++ src/Controller/ApiLoginController.php
@@ -8,11 +8,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class ApiLoginController extends AbstractController
 {
     #[Route('/api/login', name: 'app_login')]
-    public function login(Request $request, TokenStorageInterface $tokenStorage): JsonResponse
+    public function login(Request $request, TokenStorageInterface $tokenStorage, JWTTokenManagerInterface $JWTManager): JsonResponse
     {
         $token = $tokenStorage->getToken();
 
@@ -25,14 +26,26 @@ class ApiLoginController extends AbstractController
 
         $jwt = substr($jwt, 7); // Supprimer "Bearer " pour obtenir le token JWT
 
+        $csrfToken = bin2hex(random_bytes(32)); // Générer un token CSRF aléatoire
+
         /** @var User $user */
         $user = $token->getUser();
 
-        $response = new JsonResponse([
+        $tokenData = [
+            'user_id' => $user->getId(),
+            'username' => $user->getUsername(),
+            'csrf' => $csrfToken, // Ajouter le token CSRF au payload
+        ];
+
+        $token = $JWTManager->createFromPayload($user, $tokenData);
+
+        $response = new JsonResponse([ // Construire la réponse avec le token CSRF
+            'csrfToken' => $csrfToken, // Token CSRF envoyé dans la réponse
             'username' => $user->getLogin(),
             'user_id' => $user->getId(),
             'role' => $user->getRoles(),
         ]);
+        $response->headers->setCookie(new Cookie('access_token', $token, 0, '/', null, true, true, false, 'Strict'));
 
         return $response;
     }
