d09c901c5c14a0c8993c1277caef4880faf79ef9
JoAnisky <anisky5@gmail.com>
2024-06-12 15:45:59 +0200

    CSRF Token handle

--- src/Service/JWTAuthenticatorService.php
+++ src/Service/JWTAuthenticatorService.php
@@ -18,27 +18,38 @@ class JWTAuthenticatorService
 
     public function authenticate(Request $request, array $requiredRoles = ['ROLE_ADMIN', 'ROLE_GAMEMASTER'])
     {
-        // Récupérer le token JWT de l'en-tête Authorization
-        $authorizationHeader = $request->headers->get('Authorization');
-    
-        if (!$authorizationHeader || !str_starts_with($authorizationHeader, 'Bearer ')) {
-            throw new AuthenticationException('Token manquant ou invalide');
+
+        if (!$request->cookies || !$request->cookies->get('access_token')) {
+            throw new AccessDeniedException('Missing token in cookie');
         }
-    
-        // Supprimer "Bearer " pour obtenir le token JWT
-        $jwt = substr($authorizationHeader, 7);
-    
+
+        // Retrieve the JWT cookie from the request
+        $accessToken  = $request->cookies->get('access_token');
+
+        if (!$request->headers || !$request->headers->get('x-xsrf-token')) {
+            throw new AccessDeniedException('Missing XSRF token in headers');
+        }
+
+        // Récupérer le token CSRF dans les entêtes de la requete
+        $csrfToken = $request->headers->get('x-xsrf-token');
+
         try {
-            $payload = $this->jwtEncoder->decode($jwt);
-    
-            // Vérifier les rôles de l'utilisateur stockés dans le payload
+            // Decode the JWT
+            $payload = $this->jwtEncoder->decode($accessToken);
+
+            // Compare the CSRF token from the header with the one in the JWT payload
+            if ($payload['csrf'] !== $csrfToken) {
+                throw new AccessDeniedException('CSRF token mismatch');
+            }
+
+            // Verify user Roles
             $userRoles = $payload['roles'];
             foreach ($requiredRoles as $role) {
                 if (in_array($role, $userRoles)) {
                     return;
                 }
             }
-    
+
             throw new AccessDeniedException('Accès non autorisé pour ce rôle.');
         } catch (\Exception $e) {
             throw new AuthenticationException('Token invalide.');
