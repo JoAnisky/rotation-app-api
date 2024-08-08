8330b44a4cb826d48ad4c6c9d860deabb353e237
JoAnisky <anisky5@gmail.com>
2024-08-08 11:50:16 +0200

    Refactor: Inject dependencies into the constructor and generate an empty scenario when creating a new activity.

--- src/Controller/ActivityController.php
+++ src/Controller/ActivityController.php
@@ -4,9 +4,8 @@ namespace App\Controller;
 
 use App\Annotation\Secure;
 use App\Entity\Activity;
+use App\Entity\Scenario;
 use App\Repository\ActivityRepository;
-use App\Repository\StandRepository;
-use App\Repository\TeamRepository;
 use App\Repository\UserRepository;
 use App\Service\CodeGeneratorService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -16,47 +15,61 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
 #[Route('/activity')]
 class ActivityController extends AbstractController
 {
+    private $activityRepository;
+    private $serializer;
+    private $em;
+    private $validator;
+
+    public function __construct(
+        ActivityRepository $activityRepository, 
+        SerializerInterface $serializer,
+        EntityManagerInterface $em,
+        ValidatorInterface $validator,
+        ){
+            $this->activityRepository = $activityRepository;
+            $this->serializer = $serializer;
+            $this->em = $em;
+            $this->validator = $validator;
+    }
 
     #[Route('/', name: 'activity', methods: ['GET'])]
     #[Secure(roles: ["ROLE_ADMIN", "ROLE_GAMEMASTER"])]
-    public function getActivitiesList(ActivityRepository $activityRepository, SerializerInterface $serializer): JsonResponse
+    public function getActivitiesList(): JsonResponse
     {
-        $activitiesList = $activityRepository->findAll();
-        $jsonActivitiesList = $serializer->serialize($activitiesList, 'json', ['groups' => 'getActivityNameAndId']);
+        $activitiesList = $this->activityRepository->findAll();
+        $jsonActivitiesList = $this->serializer->serialize($activitiesList, 'json', ['groups' => 'getActivityNameAndId']);
         return new JsonResponse($jsonActivitiesList, Response::HTTP_OK, [], true);
     }
 
     #[Route('/{id}', name: 'detail_activity', methods: ['GET'])]
-    public function getDetailActivity(Activity $activity, SerializerInterface $serializer): JsonResponse
+    public function getDetailActivity(Activity $activity): JsonResponse
     {
         // If no activity ParamConverter will throw an Exception
         // Turn $activity object into JSON format
-        $jsonActivity = $serializer->serialize($activity, 'json', ['groups' => 'getActivity']);
+        $jsonActivity = $this->serializer->serialize($activity, 'json', ['groups' => 'getActivity']);
         return new JsonResponse($jsonActivity, Response::HTTP_OK, [], true);
     }
 
     #[Route('/{id}/stands', name: 'activity_stands', methods: ['GET'])]
-    public function getActivityStands(Activity $activity, SerializerInterface $serializer): JsonResponse
+    public function getActivityStands(Activity $activity): JsonResponse
     {
         // Serialization logic here
         $stands = $activity->getStands();
-        $jsonStands = $serializer->serialize($stands, 'json');
+        $jsonStands = $this->serializer->serialize($stands, 'json');
         return new JsonResponse($jsonStands, Response::HTTP_OK, [], true);
     }
 
     #[Route('/{id}/teams', name: 'activity_teams', methods: ['GET'])]
-    public function getActivityTeams(Activity $activity, SerializerInterface $serializer): JsonResponse
+    public function getActivityTeams(Activity $activity): JsonResponse
     {
-        // Serialization logic here
         $teams = $activity->getTeams();
-        $jsonTeams = $serializer->serialize($teams, 'json');
+        $jsonTeams = $this->serializer->serialize($teams, 'json');
         return new JsonResponse($jsonTeams, Response::HTTP_OK, [], true);
     }
 
@@ -64,21 +77,18 @@ class ActivityController extends AbstractController
      * Gets an activity by pin code
      * @param string $routePrefix API endpoint for role
      * @param string $pincode Code to check
-     * @param SerializerInterface $serializer
-     * @param ActivityRepository $activityRepository
      * @return JsonResponse
      */
     #[Route(['/{routePrefix}/code/{pincode}'], name: 'activity_pincode', methods: ['GET'])]
-    public function getActivityAndRoleByPinCode(string $routePrefix, string $pincode, SerializerInterface $serializer, ActivityRepository $activityRepository): JsonResponse
+    public function getActivityAndRoleByPinCode(string $routePrefix, string $pincode): JsonResponse
     {
-
         switch ($routePrefix) {
             case 'participant':
-                $result = $activityRepository->findBy(['participantCode' => $pincode]);
+                $result = $this->activityRepository->findBy(['participantCode' => $pincode]);
                 $role = 'participant';
                 break;
             case 'animator':
-                $result = $activityRepository->findBy(['animatorCode' => $pincode]);
+                $result = $this->activityRepository->findBy(['animatorCode' => $pincode]);
                 $role = 'animator';
                 break;
             default:
@@ -90,7 +100,7 @@ class ActivityController extends AbstractController
             return new JsonResponse(['message' => 'Activité non trouvée'], JsonResponse::HTTP_NOT_FOUND);
         }
         $role = strtoupper($role);
-        $data = $serializer->serialize([
+        $data = $this->serializer->serialize([
             'activity_id' => $result[0]->getId(),
             'role' => 'ROLE_' . $role
         ], 'json');
@@ -99,28 +109,24 @@ class ActivityController extends AbstractController
     }
 
     /**
-     * @param Activity $activity
-     * @param StandRepository $standRepository
-     * @param TeamRepository $teamRepository
-     * @param EntityManagerInterface $em
      * @return JsonResponse
      */
     #[Route('/delete/{id}', name: 'delete_activity', methods: ['DELETE'])]
     #[Secure(roles: ["ROLE_ADMIN", "ROLE_GAMEMASTER"])]
-    public function deleteActivity(Activity $activity, EntityManagerInterface $em): JsonResponse
+    public function deleteActivity(Activity $activity): JsonResponse
     {
-        $em->getConnection()->beginTransaction(); // Start transaction
+        $this->em->getConnection()->beginTransaction(); // Start transaction
 
         try {
 
-            $em->remove($activity);
-            $em->flush(); // Persist changes
+            $this->em->remove($activity);
+            $this->em->flush(); // Persist changes
 
-            $em->getConnection()->commit(); // Commit transaction
+            $this->em->getConnection()->commit(); // Commit transaction
 
             return new JsonResponse(null, Response::HTTP_NO_CONTENT);
         } catch (\Exception $e) {
-            $em->getConnection()->rollBack(); // Roll back on error
+            $this->em->getConnection()->rollBack(); // Roll back on error
 
             // Return a JsonResponse indicating an error
             return new JsonResponse([
@@ -131,7 +137,7 @@ class ActivityController extends AbstractController
     }
 
     /**
-     * Create a Activity using the following structure 
+     * Create an Activity (and empty Scenario) using the following structure 
      * 
      * {
      *  "name" : "activityName",
@@ -140,19 +146,16 @@ class ActivityController extends AbstractController
      * 
      * @param Request $request
      * @param UserRepository $userRepository
-     * @param SerializerInterface $serializer
      * @param EntityManagerInterface $em
-     * @param ValidatorInterface $validator
      * @param CodeGeneratorService $codeGenerator
      * @return JsonResponse
      */
     #[Route('/', name: 'create_activity', methods: ['POST'])]
     #[Secure(roles: ["ROLE_ADMIN", "ROLE_GAMEMASTER"])]
-    public function createActivity(Request $request, UserRepository $userRepository, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator, CodeGeneratorService $codeGenerator): JsonResponse
+    public function createActivity(Request $request, UserRepository $userRepository, CodeGeneratorService $codeGenerator): JsonResponse
     {
-
         // Create new activity object with data provided
-        $activity = $serializer->deserialize($request->getContent(), Activity::class, 'json');
+        $newActivity = $this->serializer->deserialize($request->getContent(), Activity::class, 'json');
 
         // Decode request content
         $requestData = json_decode($request->getContent(), true);
@@ -164,27 +167,32 @@ class ActivityController extends AbstractController
                 return new JsonResponse(['error' => 'User not found'], Response::HTTP_NOT_FOUND);
             }
             // Associate found User with the new Activity
-            $activity->setUser($user);
+            $newActivity->setUser($user);
         }
 
         // Générer les codes pour les participants et les animateurs
         $participantCode = $codeGenerator->generateUniqueCode('participantCode');
         $animatorCode = $codeGenerator->generateUniqueCode('animatorCode');
-        $activity->setParticipantCode($participantCode);
-        $activity->setAnimatorCode($animatorCode);
+        $newActivity->setParticipantCode($participantCode);
+        $newActivity->setAnimatorCode($animatorCode);
 
         // Validate the Activity entity before flush
-        $errors = $validator->validate($activity);
+        $errors = $this->validator->validate($newActivity);
         if (count($errors) > 0) {
-            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+            return new JsonResponse($this->serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
         }
 
+        // If everything is ok, generate new empty Scenario
+        $scenario = new Scenario();
+        $scenario->setActivity($newActivity);
         // Persist and flush new Activity
-        $em->persist($activity);
-        $em->flush();
+        $this->em->persist($newActivity);
+        $this->em->persist($scenario);
+
+        $this->em->flush();
 
         // Return a JSON response with the ID of the newly created Activity
-        return new JsonResponse(['activity_id' => $activity->getId()], JsonResponse::HTTP_CREATED);
+        return new JsonResponse(['activity_id' => $newActivity->getId()], JsonResponse::HTTP_CREATED);
     }
 
     /** PUT an existing activity
@@ -195,31 +203,26 @@ class ActivityController extends AbstractController
      * }
      * 
      * @param Request $request
-     * @param Activity $currentActivity
-     * @param SerializerInterface $serializer
-     * @param EntityManagerInterface $em
-     * @param UrlGeneratorInterface $urlGenerator
-     * @param ValidatorInterface $validator
      * @return JsonResponse
      */
     #[Route('/{id}', name: 'update_activity', methods: ['PUT'])]
     #[Secure(roles: ["ROLE_ADMIN", "ROLE_GAMEMASTER"])]
-    public function updateActivity(Request $request, Activity $currentActivity, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator): JsonResponse
+    public function updateActivity(Request $request, Activity $activity): JsonResponse
     {
 
         // Deserialization and Update Activity
-        $serializer->deserialize($request->getContent(), Activity::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentActivity]);
+        $this->serializer->deserialize($request->getContent(), Activity::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $activity]);
 
         /* The updated activity object is validated using the ValidatorInterface to ensure the data is valid.
         *  If any errors are found, a JSON response with the errors is returned with a HTTP_BAD_REQUEST status*/
-        $errors = $validator->validate($currentActivity);
+        $errors = $this->validator->validate($activity);
 
         if ($errors->count() > 0) {
             // Srialize data
-            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+            return new JsonResponse($this->serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
         }
 
-        $em->flush();
+        $this->em->flush();
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
 }
